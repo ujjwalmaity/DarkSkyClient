4296110071bb61f0df4a142e386f08a560194605
ujjwal <ujjwalmaity98@gmail.com>
2019-09-20 16:34:23 +0530

    Show summery and icon

--- app/src/main/java/dev/ujjwal/darkskyclient/MainActivity.java
+++ app/src/main/java/dev/ujjwal/darkskyclient/MainActivity.java
@@ -3,12 +3,16 @@ package dev.ujjwal.darkskyclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.main_tempTextView)
     TextView tempTextView;
+
+    @BindView(R.id.main_summeryTextView)
+    TextView summeryTextView;
+
+    @BindView(R.id.main_iconImageView)
+    ImageView iconImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +58,21 @@ public class MainActivity extends AppCompatActivity {
     public void onWeatherEvent(WeatherEvent weatherEvent) {
         Currently currently = weatherEvent.getWeather().getCurrently();
         tempTextView.setText(String.valueOf(Math.round(currently.getTemperature())));
+        summeryTextView.setText(currently.getSummary());
+
+        Map<String, Integer> iconMap = new HashMap<>();
+        iconMap.put("clear-day", R.drawable.ic_clear_day);
+        iconMap.put("clear-night", R.drawable.ic_clear_night);
+        iconMap.put("rain", R.drawable.ic_rain);
+        iconMap.put("snow", R.drawable.ic_snow);
+        iconMap.put("wind", R.drawable.ic_wind);
+        iconMap.put("fog", R.drawable.ic_fog);
+        iconMap.put("cloudy", R.drawable.ic_cloudy);
+        iconMap.put("partly-cloudy-day", R.drawable.ic_partly_cloudy_day);
+        iconMap.put("partly-cloudy-night", R.drawable.ic_partly_cloudy_night);
+        iconMap.put("thunderstorm", R.drawable.ic_thunderstorm);
+
+        iconImageView.setImageResource(iconMap.get(currently.getIcon()));
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
